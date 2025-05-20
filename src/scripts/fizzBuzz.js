8702bf105fee39edd20b4b18ff4defbea678fdc4
DiegoAPaez <diego.p31@hotmail.com>
2025-05-20 11:36:38 +0200

    Implemented logic in fizzBuzz

--- src/scripts/fizzBuzz.js
+++ src/scripts/fizzBuzz.js
@@ -5,8 +5,55 @@
         status: "", // Código indicando éxito o error
         message: "", // Mensaje
         data: {
-            number: 0, // Número evaluado
-            result: "" // Resultado esperado
+            input: 0, // Número evaluado
+            output: "" // outputado esperado
             }
     }
- * */ 
+ * */
+
+export const fizzBuzz = (userInput) => {
+    try {
+        const response = {
+            status: "",
+            message: "",
+            data: {
+                input: 0,
+                output: "",
+            },
+        };
+
+        if (typeof userInput !== "number") {
+            response.status = "Bad Request";
+            response.message = "Input cannot be processed";
+            response.data.input = userInput;
+            response.data.output = "Invalid input";
+            return response;
+        }
+
+        if (userInput % 5 === 0 && userInput % 3 === 0) {
+            response.status = "ok";
+            response.message = "Número divisible por 3 y 5";
+            response.data.input = userInput;
+            response.data.output = "FizzBuzz";
+        } else if (userInput % 5 === 0 && userInput % 3 !== 0) {
+            response.status = "ok";
+            response.message = "Número divisible por 5";
+            response.data.input = userInput;
+            response.data.output = "Buzz";
+        } else if (userInput % 5 !== 0 && userInput % 3 === 0) {
+            response.status = "ok";
+            response.message = "Número divisible por 3";
+            response.data.input = userInput;
+            response.data.output = "Fizz";
+        } else {
+            response.status = "ok";
+            response.message = "Número no divisible por 3 ni 5";
+            response.data.input = userInput;
+            response.data.output = userInput;
+        }
+        console.log({ response });
+        return response;
+    } catch (error) {
+        console.error(`Input error: ${error}`);
+    }
+};
