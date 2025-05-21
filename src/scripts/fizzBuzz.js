74ec5161b64cf70f35ee9d0a51cddb34a4315f75
DiegoAPaez <diego.p31@hotmail.com>
2025-05-21 10:19:03 +0200

    Improved logic to reduce code lines

--- src/scripts/fizzBuzz.js
+++ src/scripts/fizzBuzz.js
@@ -1,58 +1,32 @@
-/**
- * Responsabilidad: gestionar la lógica de FizzBuzz
- * Recuerda que la estructura de las respuestas debe ser estructurada:
- *  {
-        status: "", // Código indicando éxito o error
-        message: "", // Mensaje
-        data: {
-            input: 0, // Número evaluado
-            output: "" // outputado esperado
-            }
-    }
- * */
-
 export const fizzBuzz = (userInput) => {
-    try {
-        let response = {
-            status: "",
-            message: "",
-            data: {
-                input: 0,
-                output: "",
-            },
-        };
+    let response = {
+        status: "ok",
+        message: "",
+        data: {
+            input: userInput,
+            output: "",
+        },
+    };
 
-        if (typeof userInput !== "number") {
-            response.status = "Bad Request";
-            response.message = "Input cannot be processed";
-            response.data.input = userInput;
-            response.data.output = "Invalid input";
-            return response;
-        }
-
-        if (userInput % 5 === 0 && userInput % 3 === 0) {
-            response.status = "ok";
-            response.message = "Número divisible por 3 y 5";
-            response.data.input = userInput;
-            response.data.output = "FizzBuzz";
-        } else if (userInput % 5 === 0 && userInput % 3 !== 0) {
-            response.status = "ok";
-            response.message = "Número divisible por 5";
-            response.data.input = userInput;
-            response.data.output = "Buzz";
-        } else if (userInput % 5 !== 0 && userInput % 3 === 0) {
-            response.status = "ok";
-            response.message = "Número divisible por 3";
-            response.data.input = userInput;
-            response.data.output = "Fizz";
-        } else {
-            response.status = "ok";
-            response.message = "Número no divisible por 3 ni 5";
-            response.data.input = userInput;
-            response.data.output = userInput;
-        }
+    if (typeof userInput !== "number") {
+        response.status = "Bad Request";
+        response.message = "Input cannot be processed";
+        response.data.output = "Invalid input";
         return response;
-    } catch (error) {
-        console.error(`Input error: ${error}`);
     }
+
+    if (userInput % 5 === 0 && userInput % 3 === 0) {
+        response.message = "Número divisible por 3 y 5";
+        response.data.output = "FizzBuzz";
+    } else if (userInput % 3 === 0) {
+        response.message = "Número divisible por 3";
+        response.data.output = "Fizz";
+    } else if (userInput % 5 === 0) {
+        response.message = "Número divisible por 5";
+        response.data.output = "Buzz";
+    } else {
+        response.message = "Número no divisible por 3 ni 5";
+        response.data.output = userInput;
+    }
+    return response;
 };
