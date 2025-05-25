80310a20ba4d7c3fdf9c3e5dc2a7ae09caf9842a
DiegoAPaez <diego.p31@hotmail.com>
2025-05-25 10:19:18 +0200

    fizzBuzz logic moved to a try catch block to manage errors

--- src/scripts/fizzBuzz.js
+++ src/scripts/fizzBuzz.js
@@ -8,25 +8,34 @@ export const fizzBuzz = (userInput) => {
         },
     };
 
-    if (typeof userInput !== "number") {
+    try {
+        if (
+            typeof userInput !== "number" ||
+            !Number.isInteger(userInput) ||
+            userInput < 0
+        ) {
+            throw new Error("Only positive integer numbers can be submitted");
+        }
+
+        if (userInput % 5 === 0 && userInput % 3 === 0) {
+            response.message = "Número divisible por 3 y 5";
+            response.data.output = "FizzBuzz";
+        } else if (userInput % 3 === 0) {
+            response.message = "Número divisible por 3";
+            response.data.output = "Fizz";
+        } else if (userInput % 5 === 0) {
+            response.message = "Número divisible por 5";
+            response.data.output = "Buzz";
+        } else {
+            response.message = "Número no divisible por 3 ni 5";
+            response.data.output = userInput;
+        }
+        return response;
+    } catch (error) {
+        console.error(`Error on input: ${error}`);
         response.status = "Bad Request";
         response.message = "Input cannot be processed";
         response.data.output = "Invalid input";
         return response;
     }
-
-    if (userInput % 5 === 0 && userInput % 3 === 0) {
-        response.message = "Número divisible por 3 y 5";
-        response.data.output = "FizzBuzz";
-    } else if (userInput % 3 === 0) {
-        response.message = "Número divisible por 3";
-        response.data.output = "Fizz";
-    } else if (userInput % 5 === 0) {
-        response.message = "Número divisible por 5";
-        response.data.output = "Buzz";
-    } else {
-        response.message = "Número no divisible por 3 ni 5";
-        response.data.output = userInput;
-    }
-    return response;
 };
